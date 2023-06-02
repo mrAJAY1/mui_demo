666e4c9518bcd6c0e785a5c1ce0199f3fa6ebdd5
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-02 23:03:48 +0530

    Mui CheckBox

--- src/components/MuiCheckBox.tsx
+++ src/components/MuiCheckBox.tsx
@@ -1,11 +1,20 @@
 import { Box, FormControlLabel, Checkbox } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 function MuiCheckBox() {
+  const [acceptTnC, setAcceptTnC] = useState<boolean>(false);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAcceptTnC(event.target.checked);
+  };
+  console.log(acceptTnC);
   return (
     <Box>
       <Box>
-        <FormControlLabel control={<Checkbox />} label="I accept the terms and conditions" />
+        {/* Basic Checkbox */}
+        <FormControlLabel
+          control={<Checkbox onChange={handleChange} checked={acceptTnC} />}
+          label="I accept the terms and conditions"
+        />
       </Box>
     </Box>
   );
